e57aed972bd2662af3fb5eb97555c1fecd849b5d
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-29 18:46:00 -0600

    initial commit

---
+++ lib/fruit.rb
@@ -0,0 +1,11 @@
+class Fruit
+  attr_reader :name, :age, :type
+
+  def initialize(name, age, type)
+    @name = name
+    @age = age
+    @type = type
+  end
+
+
+end
---
+++ spec/fruit_spec.rb
@@ -0,0 +1,12 @@
+require 'fruit'
+RSpec.describe Fruit do
+  before(:each) do
+    @banana = Fruit.new("banana_best",7,"banana")
+  end
+
+  describe 'exists' do
+      it 'is an object' do
+       #method goes here
+      end
+  end
+end
